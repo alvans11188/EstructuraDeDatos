7e1e423433bbf88edc46419b9fb1091378fd0e1d
Alexander <achicallag@unjbg.edu.pe>
2025-04-17 20:00:48 -0500

    Añadiendo notas para explicar la secuencia de pasos

--- 2ListasEnlazadas/2.1OperacionesCreacionPorElInicio.cpp
+++ 2ListasEnlazadas/2.1OperacionesCreacionPorElInicio.cpp
@@ -1,6 +1,13 @@
 /*2.1Creacion por el inicio
 Primero se crea un punto de cabecera que apunte a null indicando que esta vacio
 q->inf // es un atributo
+Los pasos a seguir son los siguientes 
+ 1- Creo mi variable cabecera para asegurarme que apunte a null
+ 2- despues de crear q se creara un nuevo nodo
+ 3- luego de crear el nodo se carga la informacion 
+ 4- haremos que q->sig = p
+ 5- y hacer que p=q
+ 6- escribir si quieres mas datos S/N
 */
 #include<iostream>
 using namespace std;
