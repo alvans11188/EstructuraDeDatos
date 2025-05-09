cb700e9a75e3cad837bb0f800e80bdf0fa8700f1
Alvans <achicallag@unjbg.edu.pe>
2025-05-09 11:14:09 -0500

    Agregando codigo de la practica

---
+++ 5PilasConListasEnlazadas/PilasConListasEnlazadas.cpp
@@ -0,0 +1,135 @@
+#include<iostream>
+using namespace std;
+struct nodo {
+	int info;
+	nodo* liga;
+};
+class pila{
+	private:
+		nodo* tope;
+	public:
+		pila(){
+			tope=NULL;
+		}
+		//metodo para agregar un elemento a la pila
+		void agregarPila(int dato){
+			nodo* q = new nodo;
+			q->info=dato;
+			q->liga=tope;
+			tope=q;
+		}
+		//metodo para eliminar (sacar) el elemento superior de la pila
+		void sacarPila(){
+			if(tope!=NULL){
+				nodo* q=tope;
+				tope=q->liga;
+				delete q;
+			}
+			else
+			{
+				cout<<"Pila vacia, no se puede sacar elemento. "<<endl;
+				return;
+			}
+		}
+		void mostrarPila(){
+			if(tope!=NULL){
+				nodo* q=tope;
+				cout<<"Los elementos de la pila son: "<<endl;
+				while(q!=NULL){
+					cout<<q->info<<" ";
+					q=q->liga;
+				}
+				cout<<endl;
+			}
+			else
+			{
+				cout<<"Pila vacia. "<<endl;
+				return;
+			}
+			
+		}
+		
+		int contarElementosPila(){
+			
+		}
+		bool buscarElementoPila(int dato){
+			
+		}
+		bool compararCon(pila& pila2){
+			
+		}
+		
+		~pila(){
+			while(tope!=NULL){
+				sacarPila();
+			}
+		}
+};
+void menu(){
+	pila pila;
+	pila pila2;
+	int opcion, dato;
+	
+	do{
+		cout<<"\n---MENU PILA---"<<endl;
+		cout<<"1. Apilar en Pila 1"<<endl;
+		cout<<"2. Desapilar en Pila 1"<<endl;
+		cout<<"3. Mostrar Pila 1"<<endl;
+		cout<<"4. Contar elementos de Pila 1"<<endl;
+		cout<<"5. Buscar un elemento en Pila 1"<<endl;
+		cout<<"6. Apilar en Pila 2"<<endl;
+		cout<<"7. Mostrar Pila 2"<<endl;
+		cout<<"8. Comparar Pila 1 con Pila 2"<<endl;
+		cout<<"0. Salir"<<endl;
+		cout<<"Opcion: ";
+		cin>>opcion;
+		switch(opcion){
+			case 1:
+				cout<<"Ingrese dato a apilar en Pila1: ";
+				cin>>dato;
+				pila.agregarPila(dato);
+				break;
+			case 2:
+				pila.sacarPila();
+				break;
+			case 3:
+				pila.mostrarPila();
+				break;
+			case 4:
+				cout<<"Cantidad de elementos en Pila 1: "<<pila.contarElementosPila()<<endl;
+				break;
+			case 5:
+				cout<<"Ingrese el elemento a buscar en Pila 1: ";
+				cin>>dato;
+				pila.buscarElementoPila(dato);
+				cout<<"¿Esta el "<<dato<<"?: "<<(pila.buscarElementoPila(dato)?"Si":"No")<<endl;
+				break;
+			case 6:
+				cout<<"Ubgrese dato a apilar en Pila 2: ";
+				cin>>dato;
+				pila2.agregarPila(dato);
+				break;
+			case 7:
+				pila2.mostrarPila(dato);
+				break;
+			case 8:
+				if(pila.compararCon(pila2)){
+					cout<<"Las pilas son iguales. "<<endl;
+				}
+				else{
+					cout<<"Laas pilas son diferentes. "<<endl;
+				}
+				break;
+			case 0:
+				cout<<"Porgrama finalizado. "<<endl;
+				break;
+			default:
+				cout<<"Opcion invalida. "<<endl;
+		}	
+	} while(opcion!=0);
+}
+int main(){
+	setlocale(LC_ALL, "");
+	menu();
+	return 0;
+}
