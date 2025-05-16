ddbea859f56daec2f0f72d1fa4409441dc2411ef
Alvans <achicallag@unjbg.edu.pe>
2025-05-16 18:45:58 -0500

    Añadiendo descripcion y archivos de los ejercicios

---
+++ ejercicios/EjerciciosArreglos-ListasEnlazadas/10ejercicio.cpp
@@ -0,0 +1,16 @@
+/* 10 EJERCICIO
+ El problema de José: 
+ Un grupo de soldados están rodeados por una gran fuerza enemiga. No hay esperanzas de victoria si 
+no llegan refuerzos, y existe un solo caballo disponible para el escape. Los soldados se ponen de 
+acuerdo en un pacto para determinar cuál de ellos debe escapar y solicitar ayuda. Forman un círculo y 
+se escoge un número n al azar (aleatorio) de un sombrero, igualmente se escoge el nombre de un 
+soldado. Comenzando con el soldado cuyo nombre se ha seleccionado, comienzan a contar en la 
+dirección de las manecillas del reloj alrededor del círculo. Cuando la cuenta alcanza el valor de n, este 
+soldado es retirado del círculo y la cuenta empieza de nuevo, con el siguiente soldado. El proceso 
+continúa de tal manera que cada vez que la cuenta alcanza el valor de n, se remueve un soldado del 
+círculo. Un soldado que es removido del círculo, no se vuelve a contar. El último soldado que queda 
+es el que debe coger el caballo y escapar.  
+El problema consiste en: dado un número n, el ordenamiento de los hombres en el círculo y el nombre 
+a partir del cual se comienza a contar; determinar el orden en el cual los hombres son eliminados del 
+círculo y cuál hombre puede escapar. 
+*/
---
+++ ejercicios/EjerciciosArreglos-ListasEnlazadas/11ejercicio.cpp
@@ -0,0 +1,11 @@
+/* 11 EJERCICIO
+ Efectúe un programa que permita efectuar operaciones algebraicas con polinomios de una sola 
+variable: 
+ Suma 
+ Resta 
+ Multiplicación 
+ División 
+ Derivación 
+ Grado del polinomio 
+ Evaluar el valor numérico del polinomio para un valor cualquiera de la variable.
+*/
