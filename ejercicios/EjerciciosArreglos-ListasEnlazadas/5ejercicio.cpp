f00952f4e8f023d3d1b53f9c6a021bc9d0e5f4c9
Alvans <achicallag@unjbg.edu.pe>
2025-05-16 13:17:07 -0500

    Agregando ejercicios base 5

---
+++ ejercicios/EjerciciosArreglos-ListasEnlazadas/5ejercicio.cpp
@@ -0,0 +1,13 @@
+/*5 EJERCICIO
+Diseñar un algoritmo que inserte un dato ANTES DE un dato referencial en un arreglo ordenado. 
+*/
+#include<iostream>
+#define MAX 5
+using namespace std;
+
+int main(){
+	int x[MAX] = {1,2,3,4,5};
+	int n=4;
+	
+	return 0;
+}
