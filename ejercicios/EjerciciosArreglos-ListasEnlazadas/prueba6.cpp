bd78c90d2f127fcc65dbd1b4134e03b271e77405
Alvans <achicallag@unjbg.edu.pe>
2025-05-18 19:45:53 -0500

    Agregando funciones comparar pilas

--- ejercicios/EjerciciosArreglos-ListasEnlazadas/prueba6.cpp
+++ ejercicios/EjerciciosArreglos-ListasEnlazadas/prueba6.cpp
@@ -33,6 +33,47 @@ class Pila{
 				cout<<"Pila vacia"<<endl;
 			}
 		}
+		void contarElementos(){
+			nodo *q;
+			q=tope;
+			int i=0;
+			while(q!=NULL){
+				i++;
+				q=q->liga;
+			}
+			cout<<"El numero de elementos eS: "<< i <<endl;
+		}
+		void buscar_elemento(int dato){
+			nodo *q;
+			q=tope;
+			while(q->liga!=NULL&&q->inf!=dato){
+				q=q->liga;
+			}
+			if(q->inf==dato){
+				cout<<"El dato "<< dato << " si se encuentra en la pila"<<endl;
+			}else{
+				cout <<"El dato no se encuentra"<<endl;
+			}
+		}
+		void compararPilas(Pila& pila1, Pila& pila2){
+			nodo *q,*s;
+			q=pila1.tope;
+			s=pila2.tope;
+			while(q!=NULL&&s!=NULL){
+				if(q->inf!=s->inf){
+					cout<<"Las pilas son diferentes"<<endl;
+					return;
+				}
+				q=q->liga;
+				s=s->liga;
+			}
+			
+			if(q==NULL&&s==NULL){
+				cout<<"Las pilas son iguales"<<endl;
+			}else{
+				cout<<"Las pilas son diferentes"<<endl;
+			}
+		}
 };
 int main(){
 	
@@ -43,11 +84,27 @@ int main(){
 	do{
 		cin>>dato;
 		pila1.agregarPila(dato);
+		pila2.agregarPila(dato);
 		i++;
-		pila1.mostrar();
+		
 	}while(i!=3);
+	pila1.agregarPila(5);
+	pila1.mostrar(); cout<<endl;
+	pila2.mostrar();
+	//comparar con la otra pila
+	pila1.compararPilas(pila1,pila2);
 	
+	//buscar elemento pila
+	do{
+		cin>>dato;
+		pila1.buscar_elemento(dato);
+	}while(true);
 	
+
+	// contar elementos
+	pila1.contarElementos();
+	//mostrar pila
+	pila1.mostrar();
 	
 	
 	
